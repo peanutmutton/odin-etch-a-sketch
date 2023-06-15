fac25241647893378b042029c79605fdb793c2f3
PeanutMutton <peanutmutton@outlook.com>
2023-06-15 14:39:22 +0600

    Sketching and button functionality Cannot implement a strictly square grid in flex

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 function addDivs(){
     const container = document.querySelector('#container');
-    for(let i=0; i<16; i++){
+    for(let i=0; i<16*16; i++){
         const divElem = document.createElement('div');
-        divElem.textContent = i+1;
-        divElem.style.flex = "1";
+        divElem.textContent = "";
+        divElem.style.flex = "1 0 0%";
+        //divElem.style.minWidth = "100px";
+        //divElem.style.height = "100px";
+        divElem.style.alignSelf = "auto";
+
+
+
+        
+
+        divElem.addEventListener('mouseleave', () => {
+            divElem.style.background = "red";
+        })
+
         container.appendChild(divElem);
+
+
     }
 }
 
+function replaceDivs(){
+    const container = document.querySelector('#container');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    let length = window.prompt("Enter the square's length: ");
+    for(let i = 0; i < length*length; i++){
+        const divElem = document.createElement('div');
+        divElem.style.flex = "1 0 0%";
+        //divElem.style.minWidth = "100px";
+        divElem.style.height = "100px";
+        divElem.addEventListener('mouseleave', () => {
+            divElem.style.background = "red";
+        })
 
+        container.appendChild(divElem);
+    }
+}
 
 //functions:
 console.log("Hello World!")
